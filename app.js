28c1729d53b635972d28b93a8e5239e2a0a49161
Shanshan Xu <tinaxu84@gmail.com>
2021-01-25 18:56:52 -0500

    add.js

--- app.js
+++ app.js
@@ -170,20 +170,3 @@ promptUser()
   .catch(err => {
     console.log(err);
   });
-
-    // fs.writeFile('./dist/index.html', pageHTML, err => {
-    //   if (err) {
-    //     console.log(err);
-    //     return;
-    //   }
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-
-    //   fs.copyFile('./src/style.css', './dist/style.css', err => {
-    //     if (err) {
-    //       console.log(err);
-    //       return;
-    //     }
-    //     console.log('Style sheet copied successfully!');
-    //   });
-    // });
-
